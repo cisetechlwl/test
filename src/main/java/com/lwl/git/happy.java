7db72d2daccc01012630b6e7b347412822dd2bd8
eclipse_user <eclipse_user@lwl.com>
2021-07-02 12:30:27 +0800

    tests

--- src/main/java/com/lwl/git/happy.java
+++ src/main/java/com/lwl/git/happy.java
@@ -3,6 +3,6 @@ package com.lwl.git;
 public class happy {
 	public static void main(String[] args) {
 		System.out.println("test===");
-		System.out.println("test===");
+		System.out.println("liwenliang");
 	}
 }
